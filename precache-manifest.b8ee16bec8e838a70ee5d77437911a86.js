584c29a9bfda122555bea1537d82630f953dd1f0
TommysG <thomasgianx@gmail.com>
2020-09-06 15:42:30 +0300

    Updates

---
+++ precache-manifest.b8ee16bec8e838a70ee5d77437911a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356232add4e23536e62781da90dda945",
+    "url": "/calculator-1/index.html"
+  },
+  {
+    "revision": "5b493eced3129d677bd6",
+    "url": "/calculator-1/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "d6702c2a91ec9343ac14",
+    "url": "/calculator-1/static/css/main.bec202db.chunk.css"
+  },
+  {
+    "revision": "5b493eced3129d677bd6",
+    "url": "/calculator-1/static/js/2.13317995.chunk.js"
+  },
+  {
+    "revision": "d6702c2a91ec9343ac14",
+    "url": "/calculator-1/static/js/main.a15bb8d6.chunk.js"
+  },
+  {
+    "revision": "6477a5c1c93432d1d160",
+    "url": "/calculator-1/static/js/runtime~main.2c517a9e.js"
+  }
+]);
