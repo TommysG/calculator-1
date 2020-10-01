c196c75938dec08f6ee58ec72054132a7fcab06a
TommysG <thomasgianx@gmail.com>
2020-10-01 16:25:23 +0300

    Updates

---
+++ precache-manifest.febbe7070b814f35f420b832f27c8b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8390071dbf30bd72b0fa1b553726c5e7",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "5b493eced3129d677bd6",
+    "url": "/simple-calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "9d9aca6d059f4b085d23",
+    "url": "/simple-calculator/static/css/main.bec202db.chunk.css"
+  },
+  {
+    "revision": "5b493eced3129d677bd6",
+    "url": "/simple-calculator/static/js/2.13317995.chunk.js"
+  },
+  {
+    "revision": "9d9aca6d059f4b085d23",
+    "url": "/simple-calculator/static/js/main.6a6241ca.chunk.js"
+  },
+  {
+    "revision": "624ded25012065399b97",
+    "url": "/simple-calculator/static/js/runtime~main.d489fee8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calculator-1/precache-manifest.b1efc1921dc77de28ac25be90c9c99a8.js"
+  "/simple-calculator/precache-manifest.febbe7070b814f35f420b832f27c8b21.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculator-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
